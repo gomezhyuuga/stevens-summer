61feed158daaf6dcaa4ff1f6cc51bac8113d61a6
Fernando Gomez <gomezhyuuga@gmail.com>
2017-06-29 12:42:44 -0400

    React-Toolbox (Material Design) implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import  './assets/react-toolbox/theme.css'
 import './App.css';
 
+import theme from './assets/react-toolbox/theme'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
+import IconButton from 'react-toolbox/lib/button/IconButton'
+import Button from 'react-toolbox/lib/button/Button'
+import AppBar from 'react-toolbox/lib/app_bar/AppBar'
+import Layout from 'react-toolbox/lib/layout/Layout';
+import NavDrawer from 'react-toolbox/lib/drawer/Drawer';
+import Panel from 'react-toolbox/lib/layout/Panel';
+import Sidebar  from 'react-toolbox/lib/layout/Sidebar';
+
 class App extends Component {
+  state = {
+    showSidebar: true
+  }
+
+  toggleSidebar = () => {
+    this.setState({ showSidebar: !this.state.showSidebar });
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          okoko
-        </p>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Panel>
+            <AppBar title="Web visualization" />
+            <div className="flex1">
+              <Button label="Open sidebar" raised primary
+                onClick={ this.toggleSidebar }/>
+            </div>
+          </Panel>
+          <Sidebar pinned={this.state.showSidebar} width={10}>
+            <h1>
+              Navigation flow
+              <IconButton icon='close' onClick={this.toggleSidebar} />
+            </h1>
+            <div className="flex1">
+            </div>
+          </Sidebar>
+        </Layout>
+      </ThemeProvider>
     );
   }
 }
